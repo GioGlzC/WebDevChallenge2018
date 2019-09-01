5e7f25b8efee53ab58c5e174e0906d84c35bc37c
GioGlzC <54216771+GioGlzC@users.noreply.github.com>
2019-09-01 09:53:39 -0500

    Refactored code chat

--- Step2/chat.js
+++ Step2/chat.js
@@ -1,101 +1,96 @@
 <script type="text/javascript">
-  var url = window.location.href;
-  var locale = url.split("/")[5];
-  var onlineGreeting, offlineGreeting;
-  if(locale === "es") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Habla con nosotras";
-    offlineGreeting = 'Deja un mensaje';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
+ const url = window.location.href;
+const locale = url.split("/")[5];
+const onlineGreeting, offlineGreeting;
+
+//Specify the languages ​​available in our departments
+const deptos = ["es","pt","fr","hi", "pl"];
+
+$zopim(() => {
+  $zopim.livechat.departments.filter(deptos)
+})
+
+//The code that is repeated is the chat widget script. I will save it in a function to call it later when I need it
+const widgetZopim = () => {
+  window.$zopim || ((d, s) => {
+    var z = $zopim = (c) => {
+      z._.push(c)
+    }, $ = z.s =
+      d.createElement(s), e = d.getElementsByTagName(s)[0]; z.set = (o) => {
+        z.set.
+          _.push(o)
+      }; z._ = []; z.set._ = []; $.async = !0; $.setAttribute('charset', 'utf-8');
+    $.src = '//v2.zopim.com/?'; z.t = +new Date; $.
+      type = 'text/javascript'; e.parentNode.insertBefore($, e)
+  })(document, 'script');
+}
+
+
+//In the next section, I will change the remaining lines for the new function
+if (locale === deptos[0]) {
+  widgetZopim()
+  onlineGreeting = "Hola, habla con nosotras";
+  offlineGreeting = 'En este momento no podemos atenderte, deja tu mensaje y nos pondremos en contacto contigo';
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
       'online': onlineGreeting,
       'offline': offlineGreeting
-      });
     });
-  } 
-  else if(locale === "pt-pt") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Fale com a gente";
-    offlineGreeting = 'Deixe uma mensagem';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
+  });
+}
+else if (locale === deptos[1]) {
+  widgetZopim()
+  onlineGreeting = "Oi, fale com a gente";
+  offlineGreeting = 'Neste momento, não podemos ajudá-lo, deixe sua mensagem e entraremos em contato com você';
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
       'online': onlineGreeting,
       'offline': offlineGreeting
-      });
     });
-  } 
-  else if(locale === "fr") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Parlez-nous";
-    offlineGreeting = 'Laissez un message';
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
+  });
+}
+else if (locale === deptos[2]) {
+  widgetZopim()
+  onlineGreeting = "Salut, parlez-nous";
+  offlineGreeting = 'Laissez un message';
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
       'online': onlineGreeting,
       'offline': offlineGreeting
-      });
     });
-  } 
-   else if(locale === "hi") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "हमारे साथ चैट करें";
-    offlineGreeting = "हमें एक संदेश छोड़ दो";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
+  });
+}
+else if (locale === deptos[3]) {
+  widgetZopim()
+  onlineGreeting = "नमस्ते, हमारे साथ चैट करें";
+  offlineGreeting = "अब हम आपकी सहायता नहीं कर सकते। कृपया एक संदेश छोड़ें, हम आपसे बाद में संपर्क करेंगे";
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
       'online': onlineGreeting,
       'offline': offlineGreeting
-      });
     });
-  }
-   else if(locale === "pl") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Rozmawiaj z nami";
-    offlineGreeting = "Zostaw nam wiadomość";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
+  });
+}
+else if (locale === deptos[4]) {
+  widgetZopim()
+  onlineGreeting = "Cześć, Rozmawiaj z nami";
+  offlineGreeting = "Teraz nie możemy ci pomóc. proszę zostawić wiadomość, skontaktujemy się z tobą później";
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
       'online': onlineGreeting,
       'offline': offlineGreeting
-      });
     });
-  }
-  else {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Chat with us";
-    offlineGreeting = "Leave us a message";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
+  });
+}
+else {
+  widgetZopim()
+  onlineGreeting = "Hi ,chat with us";
+  offlineGreeting = "Now we cannot assist you. please leave a message, we will contact you later";
+  $zopim(function () {
+    $zopim.livechat.setGreetings({
       'online': onlineGreeting,
       'offline': offlineGreeting
-      });
     });
-  }
+  });
+}
 </script>
